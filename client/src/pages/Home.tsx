ad59d5a07d0992a776f91bffbca512f8acefd848
Lukas Volk <volk.lukas237@yahoo.co.uk>
2024-12-24 12:17:36 +0000

    Update home page UI and add menu slide out functionality

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -12,6 +12,7 @@ import {
   useChatContext,
 } from "stream-chat-react";
 import { Button } from "@/components/ui/button";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import { useNavigate } from "react-router-dom";
 
 export const Home = () => {
@@ -21,18 +22,29 @@ export const Home = () => {
 
   return (
     <Chat client={streamChat}>
-      <ChannelList
-        List={Channels}
-        sendChannelsToList
-        filters={{ members: { $in: [user.id] } }}
-      />
-      <Channel>
-        <Window>
-          <ChannelHeader />
-          <MessageList />
-          <MessageInput />
-        </Window>
-      </Channel>
+      <Sheet>
+        <ChannelList
+          List={Channels}
+          sendChannelsToList
+          filters={{ members: { $in: [user.id] } }}
+        />
+        <Channel>
+          <div className="px-6 pb-6 w-full h-screen">
+            <Window>
+              <div className="flex items-center justify-between py-4 border-b">
+                <ChannelHeader />
+
+                <Button variant="outline" asChild>
+                  <SheetTrigger>Menu</SheetTrigger>
+                </Button>
+              </div>
+
+              <MessageList />
+              <MessageInput />
+            </Window>
+          </div>
+        </Channel>
+      </Sheet>
     </Chat>
   );
 };
@@ -43,36 +55,47 @@ const Channels = ({ loadedChannels }: ChannelListMessengerProps) => {
   const { setActiveChannel, channel: activeChannel } = useChatContext();
 
   return (
-    <div className="w-60 flex flex-col gap-4 m-3 h-full">
-      <Button onClick={() => navigate("/channel/new")}>New Conversation</Button>
+    <SheetContent side="right" className="flex flex-col pt-12">
+      <Button onClick={() => navigate("/channel/new")} className="w-full">
+        New Conversation
+      </Button>
 
-      <hr className="border-grey-500" />
+      <hr className="border-gray-100" />
 
       {loadedChannels != null && loadedChannels.length > 0
         ? loadedChannels.map((channel) => {
             const isActive = channel === activeChannel;
-            const extraClasses = isActive
-              ? "bg-blue-500 text-white"
-              : "hover:bg-blue-100 bg-grey-100";
 
             return (
               <Button
                 key={channel.id}
                 onClick={() => setActiveChannel(channel)}
-                className={`p-4 rounded-lg flex gap-3 items-center ${extraClasses}`}
+                className="w-full"
                 disabled={isActive}
+                variant={isActive ? "default" : "ghost"}
               >
+                {channel.data?.image && (
+                  <img
+                    src={channel.data.image}
+                    className="w-10 h-10 rounded-full object-center object-cover"
+                  />
+                )}
                 {channel.data?.name || channel.id}
               </Button>
             );
           })
         : "No Conversations"}
 
-      <hr className="border-grey-500 mt-auto" />
+      <hr className="border-gray-100 mt-auto" />
 
-      <Button onClick={() => logout.mutate()} disabled={logout.isLoading}>
+      <Button
+        variant="secondary"
+        onClick={() => logout.mutate()}
+        disabled={logout.isPending}
+        className="w-full"
+      >
         Logout
       </Button>
-    </div>
+    </SheetContent>
   );
 };
