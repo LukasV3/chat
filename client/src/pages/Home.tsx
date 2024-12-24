3936160716a549412ff0e3977fc0895d1083f9be
Lukas Volk <volk.lukas237@yahoo.co.uk>
2024-12-24 13:20:46 +0000

    Refactor ChannelItemPreview component and update home page UI

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -9,7 +9,7 @@ import {
   MessageList,
   MessageInput,
   ChannelListMessengerProps,
-  useChatContext,
+  ChannelPreviewUIComponentProps,
 } from "stream-chat-react";
 import { Button } from "@/components/ui/button";
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
@@ -25,6 +25,7 @@ export const Home = () => {
       <Sheet>
         <ChannelList
           List={Channels}
+          Preview={ChannelItemPreview}
           sendChannelsToList
           filters={{ members: { $in: [user.id] } }}
         />
@@ -49,10 +50,12 @@ export const Home = () => {
   );
 };
 
-const Channels = ({ loadedChannels }: ChannelListMessengerProps) => {
+const Channels = ({
+  children,
+  loadedChannels,
+}: React.PropsWithChildren<ChannelListMessengerProps>) => {
   const navigate = useNavigate();
   const { logout } = useLoggedInAuth();
-  const { setActiveChannel, channel: activeChannel } = useChatContext();
 
   return (
     <SheetContent side="right" className="flex flex-col pt-12">
@@ -62,19 +65,20 @@ const Channels = ({ loadedChannels }: ChannelListMessengerProps) => {
 
       <hr className="border-gray-100" />
 
-      {loadedChannels != null && loadedChannels.length > 0
-        ? loadedChannels.map((channel) => {
-            return (
-              <ChannelItemPreview
-                key={channel.id}
-                image={channel.data?.image}
-                name={channel.data?.name}
-                isActive={channel === activeChannel}
-                handleClick={() => setActiveChannel(channel)}
-              />
-            );
-          })
-        : "No Conversations"}
+      {loadedChannels != null && loadedChannels.length > 0 ? (
+        <div>
+          <p className="text-gray-500 font-light mb-2">
+            {loadedChannels.length}{" "}
+            {loadedChannels.length === 1 ? "conversation" : "conversations"}:
+          </p>
+
+          <div className="flex flex-col gap-2">{children}</div>
+        </div>
+      ) : (
+        <p className="text-gray-500 font-light">
+          "🤷 You have no conversations... yet"
+        </p>
+      )}
 
       <hr className="border-gray-100 mt-auto" />
 
@@ -91,30 +95,37 @@ const Channels = ({ loadedChannels }: ChannelListMessengerProps) => {
 };
 
 const ChannelItemPreview = ({
-  image,
-  name,
-  isActive,
-  handleClick,
-}: {
-  image?: string;
-  name?: string;
-  isActive: boolean;
-  handleClick: () => void;
-}) => {
+  channel,
+  activeChannel,
+  displayImage,
+  displayTitle,
+  latestMessagePreview,
+  setActiveChannel,
+}: ChannelPreviewUIComponentProps) => {
+  const isActive = channel.id === activeChannel?.id;
+
   return (
     <Button
-      onClick={handleClick}
-      className="w-full"
+      onClick={() => setActiveChannel?.(channel)}
+      className="w-full h-min justify-start"
       disabled={isActive}
-      variant={isActive ? "default" : "ghost"}
+      variant={isActive ? "secondary" : "ghost"}
     >
-      {image && (
-        <img
-          src={image}
-          className="w-10 h-10 rounded-full object-center object-cover"
-        />
-      )}
-      {name || "Unnamed Channel"}
+      <div className="flex h-14 w-14 shrink-0 overflow-hidden rounded-full mr-2">
+        {displayImage ? (
+          <img src={displayImage} className="aspect-square h-full w-full" />
+        ) : (
+          <div className="flex h-full w-full items-center justify-center rounded-full bg-blue-100 text-lg">
+            {displayTitle && displayTitle[0].toUpperCase()}
+          </div>
+        )}
+      </div>
+
+      <div className="flex flex-col gap-0.5 text-left truncate">
+        <p className="font-bold">{displayTitle || "Unnamed Channel"}</p>
+
+        <p className=" text-gray-500 font-light">{latestMessagePreview}</p>
+      </div>
     </Button>
   );
 };
